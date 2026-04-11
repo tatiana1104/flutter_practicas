7f45ff09c0da4e415b9d5b55fdcd5e53c32b7917
Tatiana Vargas <jud.vargas@udla.edu.co>
2026-04-11 18:41:58 -0500

    Elimina archivos innecesarios

--- modulo_1/unidad_4/apirest/android/app/src/main/kotlin/com/example/apirest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.apirest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- modulo_1/unidad_4/dio_exercise/android/app/src/main/kotlin/com/example/dio_exercise/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dio_exercise
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
