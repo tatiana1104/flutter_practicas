744806ce18c04a53efa02693e94403bf728bd0c1
Tatiana Vargas <jud.vargas@udla.edu.co>
2026-05-29 21:32:21 -0500

    clase 1-

---
+++ Modulo_4_Unidad_7/chatbot_gemini/lib/screens/chat_screen.dart
@@ -0,0 +1,14 @@
+import '.../services/gemini_service.dart';
+
+class ChatScreen extends StatefulWidget{
+  @override
+  State<ChatScrren> createState()=> _ChatScreenState;
+}
+
+class _ChatScreenState extends State<ChatScreen>{
+
+  final TextEditingController controller = TextEditingController();
+
+  //servicio de gemini IA
+  final GeminiService geminiService 
+}
---
+++ Modulo_4_Unidad_7/chatbot_gemini/lib/services/gemini_service.dart
@@ -0,0 +1,2 @@
+//https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$apiKey"
+//https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent
